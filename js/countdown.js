59de815322583df089d4f94738c0017110df9d57
TIGERIZ <tigerizofficial@gmail.com>
2024-03-11 13:59:56 +0530

    update timer

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 11, 2024 14:00:00").getTime(); 
+var countDownDate = new Date("Mar 11, 2024 16:00:00").getTime(); 
 
 // Update the count down every 1 second
 var x = setInterval(function () {
