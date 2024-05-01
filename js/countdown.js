38500e08552db6f3975230eab1b874e8c4cb372c
LokeshLmr <166609933+LokeshLmr@users.noreply.github.com>
2024-05-01 07:43:48 +0530

    Update countdown.js
    add count

--- js/countdown.js
+++ js/countdown.js
@@ -12,7 +12,8 @@ var x = setInterval(function () {
   // Time calculations for days, hours, minutes, and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)+3
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)+24
+    
   );
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
