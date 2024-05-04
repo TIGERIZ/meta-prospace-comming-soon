152efac1790b7399c8f89316b684e4ccc491b8f7
LokeshLmr <166609933+LokeshLmr@users.noreply.github.com>
2024-05-04 10:22:49 +0530

    Update countdown.js
    update comutdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,38 +1,38 @@
 // Get today's date
 var today = new Date();
 
-// Set the countdown date to today's date at 5:30 PM
-var countDownDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 18, 30, 0).getTime();
+// Set the target time to stop the countdown today at 6:35 PM
+var stopTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0).getTime();
 
-// Update the count down every 1 second
+// Update the countdown every 1 second
 var x = setInterval(function () {
   // Get the current time
   const now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-  // Time calculations for days, hours, minutes, and seconds
+  // Check if the current time has passed the stop time
+  if (now >= stopTime) {
+    clearInterval(x); // Stop the countdown
+    document.getElementById("timer").innerHTML = ` has launched successfully`;
+    document.getElementById("cong").innerHTML = `<img src="/assets/cong.gif" alt="cong"/>`;
+    document.getElementById("launching-after").style.display = "none";
+    document.getElementById("non-working").style.display = "none";
+    return; // Exit the function
+  }
+
+  // Find the remaining time
+  var distance = stopTime - now;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor(
     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-
   );
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result
+
+
+  // Display the remaining time
   document.getElementById("days").innerHTML = days.toString().length < 2 ? "0" + days.toString() : days.toString();
   document.getElementById("hours").innerHTML = hours.toString().length < 2 ? "0" + hours.toString() : hours.toString();
   document.getElementById("minutes").innerHTML = minutes.toString().length < 2 ? "0" + minutes.toString() : minutes.toString();
   document.getElementById("seconds").innerHTML = seconds.toString().length < 2 ? "0" + seconds.toString() : seconds.toString();
-
-  // If the count down is finished, display text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = ` has launched successfully`;
-    document.getElementById("cong").innerHTML = `<img src="/assets/cong.gif" alt="cong"/>`;
-
-    document.getElementById("launching-after").style.display = "none";
-    document.getElementById("non-working").style.display = "none";
-  }
 }, 1000);
